f8e2cf0c74073308ad499d05b57ee6e99a070995
neokey <neokeeeyyy@users.noreply.github.com>
2026-07-13 20:15:38 -0600

    fix: remove ktor-client-sse (integrated in core for Ktor 3.x)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,6 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.json)
-    implementation(libs.ktor.client.sse)
     implementation(libs.ktor.client.websockets)
     implementation(libs.ktor.client.logging)
 
