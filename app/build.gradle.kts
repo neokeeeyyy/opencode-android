984abf51b01efc5341ccd5c5b2375779ee7c8a69
neokey <neokeeeyyy@users.noreply.github.com>
2026-07-13 21:33:11 -0600

    fix: exclude Netty META-INF packaging conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,13 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/INDEX.LIST"
+            excludes += "/META-INF/NOTICE"
+            excludes += "/META-INF/LICENSE"
+            excludes += "/META-INF/NOTICE.txt"
+            excludes += "/META-INF/LICENSE.txt"
+            excludes += "/META-INF/io.netty.versions.properties"
+            excludes += "/META-INF/MANIFEST.MF"
         }
     }
 }
